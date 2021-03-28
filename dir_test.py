ac668943b0aa72c7813fd086bfe5e8f329f6e237
extal <extalionas@gmail.com>
2021-03-29 01:09:19 +0200

    sound test

--- dir_test.py
+++ dir_test.py
@@ -1,5 +1,12 @@
-import turtle
-print (dir(turtle))
+from gtts import gTTS
+from playsound import playsound
 
+audio = "speech.mp3"
+language = 'ru'
+example= input("enter текст\n")
 
+sp = gTTS(text= example, lang = language,slow=False)
+
+sp.save(audio)
+playsound(audio)
 
